54c082ad6d95316b8c1159c176371c2c9349b1b4
jAzzvdou <joaoazevedo592@gmail.com>
2023-12-27 14:40:52 -0300

    Pipex

--- utils.c
+++ utils.c
@@ -9,3 +9,59 @@ void	*ft_memset(void *s, int c, size_t n)
 		zero_s[n] = c;
 	return ((void *)zero_s);
 }
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *s)
+{
+	int		i;
+	char	*final;
+
+	final = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (final == NULL)
+		return (NULL);
+	i = -1;
+	while (s[++i])
+		final[i] = s[i];
+	final[i] = '\0';
+	return (final);
+}
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	if (n == 0)
+		return (0);
+	n -= 1;
+	i = -1;
+	while ((s1[++i] || s2[i]) && i < n)
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	char	*new_s;
+
+	new_s = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (new_s == NULL)
+		return (NULL);
+	i = 0;
+	while (*s1)
+		new_s[i++] = *s1++;
+	while (*s2)
+		new_s[i++] = *s2++;
+	new_s[i] = '\0';
+	return (new_s);
+}
