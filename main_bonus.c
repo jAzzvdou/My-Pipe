df8728e62d29b0fab97974d6bb6e7173ac5bc718
jAzzvdou <joaoazevedo592@gmail.com>
2024-01-08 12:31:12 -0300

    Pipex

--- main_bonus.c
+++ main_bonus.c
@@ -4,14 +4,15 @@ void	heredoc(t_pipex *pipex, char *limiter)
 {
 	char	*str_input;
 
-	while (strncmp(str_input, limiter, ft_strlen(limiter)) != 0)
+	while (strncmp(str_input, limiter, ft_strlen(str_input)) != 0)
 	{
 		write(1, "> ", 2);
-		str_input = get_next_line(1);
+		str_input = get_next_line(0);
 		write(pipex->fd[0], str_input, ft_strlen(str_input));
 		write(pipex->fd[0], "\n", 1);
 		free(str_input);
 	}
+	cmd_controller(pipex, argc, argv);
 }
 
 static int	start_heredoc(t_pipex *pipex, int argc, char **argv, char **envp)
@@ -19,7 +20,8 @@ static int	start_heredoc(t_pipex *pipex, int argc, char **argv, char **envp)
 	pipex->path = NULL;
 	pipex->cmdargs = NULL;
 	pipex->envi = envp;
-	pipex->fd[0] = open(argv[argc - 1], O_CREAT | O_RDWR | O_TRUNC, 00700);
+	pipex->fd[0] = open(".here_doc", O_CREAT | O_RDWR | O_TRUNC, 00700);
+	pipex->fd[1] = open(argv[argc - 1], O_CREAT | O_RDWR | O_TRUNC, 00700);
 	if (pipex->fd[0] < 0) 
 		return (1);
 }
@@ -44,7 +46,7 @@ int     main(int argc, char **argv, char **envp)
                 return (write(2, ".ERROR: Invalid Number Of Arguments.\n", 37));
 	if (strncpm(argv[1], "here_doc", strlen("here_doc")) == 0)
 	{
-		if (argc != 6)
+		if (argc < 6)
 			return (write(2, ".ERROR: argc != 6.\n", 19));
 		if (start_heredoc(&pipex, argc, argv, envp) != 0)
 			return (write(2, ".ERROR: start_heredoc.\n", 22));
